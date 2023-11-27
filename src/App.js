86b525895e711a0942ee12660c5b04b3d03a92aa
Eugen <worittagebez@gmail.comssh-keygen -t rsa -b 4096 -C worittagebez@gmail.com>
2023-11-27 22:42:38 +0100

    pred final project

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from "react";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import Items from "./components/items";
-
+import Categories from "./components/categories";
+import ShowFullItem from "./components/showFullItem";
 
 
 class App extends React.Component {
-  constructor(props) {
+  constructor(props) { 
     super(props)
     this.state = {
       orders: [],
+      currentItems: [],
       items: [
         {
           id: 1,
@@ -29,18 +31,18 @@ class App extends React.Component {
         },
         {
         id: 3,
-        title: 'Стол',
+        title: 'Диван',
         img: '',
         desc: 'Tudeli',
-        category: 'tables',
+        category: 'sofa',
         price: '549.99'
       },
       {
         id: 4,
-        title: 'Стол',
+        title: 'Лампа',
         img: '',
         desc: 'Tudeli',
-        category: 'tables',
+        category: 'light',
         price: '25.00'
       },
       {
@@ -48,26 +50,48 @@ class App extends React.Component {
         title: 'Стол',
         img: '',
         desc: 'Tudeli',
-        category: 'tables',
+        category: 'chairs',
         price: '149.00'
       }
-      ]
+      ],
+      showFullItem: false,
+      fullItem: {}
     }
-
+    this.state.currentItems = this.state.items
     this.addToOrder = this.addToOrder.bind(this)
     this.deleteOrder = this.deleteOrder.bind(this)
-
+    this.chooseCategory = this.chooseCategory.bind(this)
+    this.onShowItem = this.onShowItem.bind(this)
 
   }
  render() {
   return (
     <div className="wrapper">
       <Header orders={this.state.orders} onDelete={this.deleteOrder} />
-      <Items items={this.state.items} onAdd={this.addToOrder}/>
+      <Categories chooseCategory={this.chooseCategory} />
+      <Items onShowItem={this.onShowItem} items={this.state.currentItems} onAdd={this.addToOrder}/>
+
+      {this.state.showFullItem && <ShowFullItem onAdd={this.addToOrder} onShowItem={this.onShowItem} item={this.state.fullItem} />}
       <Footer />
     </div>
   )
  }
+
+ onShowItem(item) {
+  this.setState({fullItem: item})
+  this.setState({showFullItem: !this.state.showFullItem})
+ }
+
+chooseCategory(category) {
+  if(category === 'all') {
+    this.setState({currentItems: this.state.items})
+    return
+  }
+
+ this.setState({
+  currentItems: this.state.items.filter(el => el.category === category)
+ })
+}
 
  deleteOrder(id) {
   this.setState({orders: this.state.orders.filter(el => el.id !== id)})
--- src/components/showFullItem.js
+++ src/components/showFullItem.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react'
 
-export class item extends Component {
+export class ShowFullItem extends Component {
   render() {
     return (
-      <div className='item'>
-        <img src={"./image/b.jpg" } onClick={() => this.props.onShowItem(this.props.item)}  alt='tru'/>
+      <div className='full-item'>
+       <div>
+       <img src={"./image/b.jpg" } onClick={() => this.props.onShowItem(this.props.item)}  alt='tru'/>
         <h2>{this.props.item.title}</h2>
         <p>{this.props.item.desc}</p>
         <b>{this.props.item.price}</b>
         <div className='add-to-cart' onClick={() => this.props.onAdd(this.props.item)}>+</div>
+       </div>
       </div>
     )
   }
 }
 
-export default item
+export default ShowFullItem
